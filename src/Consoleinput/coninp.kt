db6cb092e22ef0ad83f76b7895557acffa943b24
Mathew <matthewcharles.tech@gmail.com>
2026-01-20 15:57:02 +0530

    toInt()

--- src/Consoleinput/coninp.kt
+++ src/Consoleinput/coninp.kt
@@ -2,10 +2,19 @@ package Consoleinput
 
 fun coninp(){
 
-    println("enter the number : " )
+   /* println("enter the number : " )
 
     val input = readln()
 
-    println(input)
+    println(input) */
+
+    println("Enter the number : ")
+
+    val input =readln().toInt()
+
+    if(input % 2 ==0)
+        println("${input} is  even number .")
+    else
+    println("${input} is odd number ")
 
 }
