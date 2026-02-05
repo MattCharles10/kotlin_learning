58a43463d74f555148069b0586c12296b91d4c61
Mathew <matthewcharles.tech@gmail.com>
2026-02-05 15:13:20 +0530

    twopass Array

--- src/Arrays/secondlargest.java
+++ src/Arrays/secondlargest.java
@@ -3,6 +3,11 @@ package Arrays;
 import java.util.Arrays;
 
 public class secondlargest {
+   /* ----------------------------------------------------------------->//Time Complexity: O(n*log(n)),
+     as sorting the array takes O(n*log(n)) time and
+     traversing the array can take O(n) time in the worst case,
+     so total time complexity = (n*log(n) + n) = O(n*log(n)).
+     Auxiliary space: O(1), as no extra space is required. */
 
     static int getsecondlargest(int arr []){
         int n = arr.length;
---
+++ src/Arrays/twopass.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class twopass {
+
+ static int Secarrr(int arr []){
+     int n = arr.length , largestarr = -1 , Secondlargest = -1;
+
+
+     //Finding largest element
+     for(int i =0 ; i< n ; i++){
+         if(arr[i] > largestarr){
+             largestarr = arr[i];
+         }
+     }
+
+     //Finding Secondlargest element
+     for(int i =0 ; i<n ; i++){
+         if(arr[i]>Secondlargest && arr[i] != largestarr){
+              Secondlargest  = arr[i] ;
+         }
+     }
+     return Secondlargest;
+
+ }
+
+ public static void main(String[] args){
+     int arr [] = {12,34,15,56,67,18};
+     System.out.println(Secarrr(arr));
+
+ }
+
+
+}
