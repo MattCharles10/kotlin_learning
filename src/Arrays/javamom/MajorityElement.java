6f5580baf41d94591c8c796e542c4c40eb4a046f
Mathew <matthewcharles.tech@gmail.com>
2026-02-13 11:18:00 +0530

    Majority element N/3

---
+++ src/Arrays/javamom/MajorityElement.java
@@ -0,0 +1,66 @@
+package Arrays.javamom;
+
+/*
+Given an array arr[] consisting of n integers, find all the array elements which occurs more than floor(n/3) times.
+Note: The returned array of majority elements should be sorted.
+
+Examples:
+
+Input: arr[] = [2, 2, 3, 1, 3, 2, 1, 1]
+Output: [1, 2]
+Explanation: The frequency of 1 and 2 is 3, which is more than floor n/3 (8/3 = 2).
+
+Input: arr[] = [-5, 3, -5]
+Output: [-5]
+Explanation: The frequency of -5 is 2, which is more than floor n/3 (3/3 = 1).
+
+Input: arr[] = [3, 2, 2, 4, 1, 4]
+Output: [ ]
+Explanation: There is no majority element.
+
+*/
+
+import java.util.*;
+
+public class MajorityElement {
+
+    static ArrayList<Integer> MajorityEle(int arr[]) {
+
+        int n = arr.length;
+
+        HashMap<Integer, Integer> Freq = new HashMap<>();
+
+        ArrayList<Integer> res = new ArrayList<>();
+
+
+        //Find the freq or count the values
+        for (int ele : arr) {
+
+            Freq.put(ele, Freq.getOrDefault(ele, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> it : Freq.entrySet()) {
+
+            int ele = it.getKey();
+            int cnt = it.getValue();
+
+            if (cnt > n / 3) {
+                res.add(ele);
+
+            }
+
+        }
+
+        Collections.sort(res);
+        return res;
+
+    }
+
+    public  static  void main(String[] args){
+        int[] arr = {2, 2, 3, 1, 3, 2, 1, 1};
+        ArrayList<Integer> r1 = MajorityEle(arr);
+        for (int ele : r1) {
+            System.out.print(ele + " ");
+        }
+    }
+}
