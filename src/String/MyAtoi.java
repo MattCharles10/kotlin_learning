8f6428ce62c15d9a8d896db52401f5d3efbea195
Mathew <matthewcharles.tech@gmail.com>
2026-02-19 12:22:47 +0530

    String to Integer - Write your own atoi()

---
+++ src/String/MyAtoi.java
@@ -0,0 +1,29 @@
+package Arrays.javamom;
+
+/*Input: s = "-123"
+Output: -123
+
+Input: s = "   -"
+Output: 0
+Explanation: No digits are present, therefore 0.
+
+Input: s = "  1231231231311133"
+Output: 2147483647
+Explanation: The converted number is greater than 231 - 1, therefore print 231 - 1 = 2147483647.
+
+Input: s = "-999999999999"
+Output: -2147483648
+Explanation: The converted number is smaller than -231, therefore print -231 = -2147483648.
+
+Input: s = "  -0012gfg4"
+Output: -12
+Explanation: Nothing is read after -12 as a non-digit character 'g' was encountered.*/
+
+public class MyAtoi {
+
+
+
+    public static void main(String [] args){
+
+    }
+}
