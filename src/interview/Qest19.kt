d2e427143268a46cb8ba396ca6667943d8508056
Mathew <matthewcharles.tech@gmail.com>
2026-03-16 21:34:37 +0530

    largest element

--- src/interview/Qest19.kt
+++ src/interview/Qest19.kt
@@ -6,5 +6,14 @@ class Qest19 {
         return if (num % 2 == 0) "Even" else "Odd"
     }
 
+    fun findlargest(arr : IntArray) : Int {
+        var max = arr[0]
+        for(i in 1 until arr.size){
+            if(arr[i] > max){
+                max = arr[i]
+            }
+        }
+        return  max
+    }
 
 }
