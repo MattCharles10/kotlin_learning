b8bba34b39989b0ba794fa0568b7c7ffabe11b12
Mathew <matthewcharles.tech@gmail.com>
2026-03-31 21:15:59 +0530

    Quest36 - NextPermuatation

---
+++ src/interview/Quest36.java
@@ -0,0 +1,74 @@
+package interview;
+
+/*
+
+Input: arr[] = [2, 4, 1, 7, 5, 0]
+Output: [2, 4, 5, 0, 1, 7]
+Explanation: The next lexicographically greater arrangement of the elements in the array arr[] is [2, 4, 5, 0, 1, 7].
+
+Input: arr[] = [3, 2, 1]
+Output: [1, 2, 3]
+Explanation: This is the last permutation, so we return the lowest possible permutation (ascending order).
+
+Input: arr[] = [1, 3, 5, 4, 2]
+Output: [1, 4, 2, 3, 5]
+Explanation: The next lexicographically greater arrangement of the elements in the array arr[] is [1, 4, 2, 3, 5].
+
+*/
+
+
+public class Quest36 {
+
+    static void nextPermutation(int arr[]) {
+
+        int n = arr.length;
+        int pivot = -1;
+
+        // Step 1: find pivot
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        // Step 2: if no pivot, reverse whole array
+        if (pivot == -1) {
+            reverse(arr, 0, n - 1);
+            return;
+        }
+
+        // Step 3: find next greater element
+        for (int i = n - 1; i > pivot; i--) {
+            if (arr[i] > arr[pivot]) {
+                swap(arr, i, pivot);
+                break;
+            }
+        }
+
+        // Step 4: reverse right side
+        reverse(arr, pivot + 1, n - 1);
+    }
+
+    static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start++, end--);
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 2, 4, 1, 7, 5, 0 };
+
+        nextPermutation(arr);
+
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
